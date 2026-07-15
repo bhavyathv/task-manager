b331a0bd90f55619c43bd3e7c4c5c4ac41820d6e
unknown <Bhavya>
2026-07-15 15:19:02 +0530

    first commit

--- app.py
+++ app.py
@@ -244,4 +244,4 @@ def toggle_status(task_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates taskmanager.db and tables if they don't exist
-    app.run(debug=True)
+    app.run(debug=True, host=0.0.0.0)
