5235d3d0630e0ce8b7b77a57c7a855f358063440
bhavyathv <bhavyakavi2007@gmail.com>
2026-07-16 15:12:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ db = SQLAlchemy(app)
 # ---------------------------------------------------------
 # DATABASE MODELS
 # ---------------------------------------------------------
-class User(db.Model):
+class User("db.Model"):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
